c8087b3f6f903fa031ed248076fc258ccf723f53
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2022-08-27 15:48:31 -0400

    Create TempDB_Move.sql

---
+++ MSSQL/TempDB_Move.sql
@@ -0,0 +1,9 @@
+declare @path as varchar(255)
+set @path = 'D:\DB\'
+
+SELECT 'ALTER DATABASE tempdb MODIFY FILE (NAME = [' + f.name + '],'
+	+ ' FILENAME = ''' + @path + f.name
+	+ CASE WHEN f.type = 1 THEN '.ldf' ELSE '.mdf' END
+	+ ''');'
+FROM sys.master_files f
+WHERE f.database_id = DB_ID(N'tempdb');
